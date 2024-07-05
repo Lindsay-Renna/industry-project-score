85d0257501e9a3917c31a61cd829be7a3bdd3b05
Lindsay Renna <lrenna.oct@gmail.com>
2024-07-04 22:21:34 -0400

    static homepage complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,25 +2,19 @@ import { useState } from "react";
 import Modal from "./components/Modal/Modal";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
 	const [modalOpen, setModalOpen] = useState(false);
 
 	return (
 		<>
-			<Header />
+			<Header setModalOpen={setModalOpen} />
 			<div className="homepage">
 				<img
 					src="/src/assets/score-homepage-cropped.png"
 					alt="score home-page"
 				/>
-				<button
-					onClick={() => {
-						setModalOpen(true);
-					}}
-				>
-					Open Modal
-				</button>
 			</div>
 			<Modal modalOpen={modalOpen} setModalOpen={setModalOpen}></Modal>
 		</>

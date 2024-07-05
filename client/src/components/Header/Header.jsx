89597ac1b256aed275323c698ba70d08c64c26fb
Lindsay Renna <lrenna.oct@gmail.com>
2024-07-04 23:01:42 -0400

    added pulse to trivia button

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,27 +3,29 @@ import "./Header.scss";
 
 function Header({ setModalOpen }) {
 	return (
-		<header className="header">
-			<div className="header__wrapper">
-				<div className="header__logo">
-					<img src="/src/assets/icons/Score-logo.svg" alt="score logo" />
+		<div className="header__container">
+			<header className="header">
+				<div className="header__wrapper">
+					<div className="header__logo">
+						<img src="/src/assets/icons/Score-logo.svg" alt="score logo" />
+					</div>
+					<NavBar />
+					<div className="header__buttons">
+						<button
+							className="header__button header__button--yellow"
+							onClick={() => {
+								setModalOpen(true);
+							}}
+						>
+							<span>Trivia Game</span>
+						</button>
+						<button className="header__button">
+							<span>Download the App</span>
+						</button>
+					</div>
 				</div>
-				<NavBar />
-				<div className="header__buttons">
-					<button
-						className="header__button header__button--yellow"
-						onClick={() => {
-							setModalOpen(true);
-						}}
-					>
-						<span>Trivia Game</span>
-					</button>
-					<button className="header__button">
-						<span>Download the App</span>
-					</button>
-				</div>
-			</div>
-		</header>
+			</header>
+		</div>
 	);
 }
 

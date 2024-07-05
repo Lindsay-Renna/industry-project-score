3639960bbfabb807166a8e268328f8ae04284f1b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-05 12:12:02 -0300

    fix the show and hide leaderboard

--- client/src/components/Leaderboard/Leaderboard.jsx
+++ client/src/components/Leaderboard/Leaderboard.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { leaderboard } from "../../lib/leaderboard";
 import { LeaderboardProfiles } from "../LeaderboardProfiles/LeaderboardProfiles";
 import ScoreLogo from "../../assets/Score-logo.svg";
+
 export const Leaderboard = () => {
   const [period, setPeriod] = useState(0);
 
@@ -13,19 +14,30 @@ export const Leaderboard = () => {
   return (
     <section className="leaderboard">
       <aside className="leaderboard__header">
-        
         <h2 className="leaderboard__heading">
           <img src={ScoreLogo} alt="Score Logo" height="20" />
           <span className="leaderboard__heading--animation">Leaderboard</span>
         </h2>
         <div className="leaderboard__duration">
-          <button onClick={handleClick}  data-id="7" className="leaderboard__duration--button">
+          <button
+            onClick={handleClick}
+            data-id="7"
+            className="leaderboard__duration--button"
+          >
             Weekly
           </button>
-          <button onClick={handleClick} data-id="30" className="leaderboard__duration--button">
+          <button
+            onClick={handleClick}
+            data-id="30"
+            className="leaderboard__duration--button"
+          >
             Monthly
           </button>
-          <button onClick={handleClick} data-id="0" className="leaderboard__duration--button">
+          <button
+            onClick={handleClick}
+            data-id="0"
+            className="leaderboard__duration--button"
+          >
             All-Time
           </button>
         </div>
@@ -39,24 +51,23 @@ export const Leaderboard = () => {
   );
 };
 
-function filterLeaderboard(data, between){
+function filterLeaderboard(data, between) {
   const today = new Date();
   const previous = new Date(today);
   previous.setDate(previous.getDate() - (between + 1));
 
-  let filter = data.filter(val => {
-      let userDate = new Date(val.dt);
-      if (between == 0) return val;
-      return previous <= userDate && today >= userDate;
-  })
+  let filter = data.filter((val) => {
+    let userDate = new Date(val.dt);
+    if (between == 0) return val;
+    return previous <= userDate && today >= userDate;
+  });
 
-  // sort with asending order
+  // sort with ascending order
   return filter.sort((a, b) => {
-      if ( a.score === b.score){
-          return b.score - a.score;
-      } else{
-          return b.score - a.score;
-      }
-  })
-
+    if (a.score === b.score) {
+      return b.score - a.score;
+    } else {
+      return b.score - a.score;
+    }
+  });
 }

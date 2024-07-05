3e326f157978f99ecb39ab230b6b6d21b9428f4f
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-04 21:42:15 -0300

    create leaderboard component, page, and scss

---
+++ client/src/components/Leaderboard/Leaderboard.jsx
@@ -0,0 +1,11 @@
+import "./Leaderboard.scss";
+
+export const Leaderboard = () => {
+    return (
+        <section className="">
+            <aside className="">
+
+            </aside>
+        </section>
+    )
+}

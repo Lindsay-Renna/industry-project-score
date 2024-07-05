5691dd171814bed0f5a409ee90fc66f06823b153
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-04 22:05:13 -0300

    style the leaderboard header

--- client/src/components/Leaderboard/Leaderboard.jsx
+++ client/src/components/Leaderboard/Leaderboard.jsx
@@ -1,19 +1,21 @@
 import "./Leaderboard.scss";
 
+import { LeaderboardProfiles } from "../LeaderboardProfiles/LeaderboardProfiles";
+
 export const Leaderboard = () => {
     return (
         <section className="leaderboard">
             <aside className="leaderboard__header">
-                <h1 className="leaderboard__heading">Leaderboard</h1>
+                <h2 className="leaderboard__heading">Leaderboard</h2>
 
                 <div className="leaderboard__duration">
-                    <button onClick={()=> {}}>Daily</button>
-                    <button onClick={()=> {}}>Weekly</button>
-                    <button onClick={()=> {}}>Monthly</button>
+                    <button className="leaderboard__duration--button" onClick={()=> {}}>Daily</button>
+                    <button className="leaderboard__duration--button" onClick={()=> {}}>Weekly</button>
+                    <button className="leaderboard__duration--button" onClick={()=> {}}>Monthly</button>
                 </div>
             </aside>
             <aside className="leaderboard__scores">
-                
+                <LeaderboardProfiles  />
             </aside>
         </section>
     )

1b86b70fbd1a323aaf96b6d91be6e137604d148a
Lindsay Renna <lrenna.oct@gmail.com>
2024-07-05 10:18:45 -0400

    wip

--- client/src/components/CollapsePanel/CollapsePanel.jsx
+++ client/src/components/CollapsePanel/CollapsePanel.jsx
@@ -3,21 +3,28 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import { Collapse, Button, CardBody, Card } from "reactstrap";
 
-function CollapsePanel({ children }) {
-	const [isOpen, setIsOpen] = useState(true);
+function CollapsePanel() {
+	const [isOpen, setIsOpen] = useState(false);
 	const toggle = () => setIsOpen(!isOpen);
 
 	return (
-		<React.StrictMode>
+		<>
 			<Button color="primary" onClick={toggle} style={{ marginBottom: "1rem" }}>
 				Toggle
 			</Button>
 			<Collapse isOpen={isOpen}>
 				<Card>
-					<CardBody>{children}</CardBody>
+					<CardBody>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
+							voluptas voluptatum eligendi voluptates tenetur cupiditate dolorum
+							at velit eius, accusamus, nesciunt quasi a, ratione quia
+							blanditiis? Qui fuga sed tempore.
+						</p>
+					</CardBody>
 				</Card>
 			</Collapse>
-		</React.StrictMode>
+		</>
 	);
 }
 
--- client/src/pages/Welcome/Welcome.jsx
+++ client/src/pages/Welcome/Welcome.jsx
@@ -1,17 +1,17 @@
-import './Welcome.scss';
-import Completion from '../Completion/Completion';
+import "./Welcome.scss";
+import Completion from "../Completion/Completion";
 
 const Welcome = () => {
-    return (
-        <section className='welcome'>
-            <div className='main'>
-                <h2 className='main__text'>Today you have 4 questions to answer!</h2>
-                <button className="main__button">
-                    <span>Get Started</span>
-                </button>
-            </div>
-        </section>
-    );
+	return (
+		<section className="welcome">
+			<div className="main">
+				<h2 className="main__text">Today you have 4 questions to answer!</h2>
+				<button className="main__button">
+					<span>Get Started</span>
+				</button>
+			</div>
+		</section>
+	);
 };
 
 export default Welcome;
